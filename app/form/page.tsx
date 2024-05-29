7c4ff23f142602ecbe7acf2f3279bf45cbbcdec4
clipper <mkopoi3@gmail.com>
2024-05-29 14:33:40 +0300

    minor changes

---
+++ app/form/page.tsx
@@ -0,0 +1,15 @@
+function FormPage() {
+  return (
+    <div
+      className="px-4 py-4 bg-violet-100 flex flex-col gap-5"
+      style={{
+        maxWidth: '1920px',
+        margin: '0 auto',
+      }}
+    >
+      hello
+    </div>
+  )
+}
+
+export default FormPage
