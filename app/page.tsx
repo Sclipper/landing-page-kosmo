0ad74e49ff767401d068b496bd63a1276e43121e
clipper <mkopoi3@gmail.com>
2024-05-31 12:07:08 +0300

    Fixed screen height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MainPage from './components/MainPage'
 export default function Home() {
   return (
     <div
-      className="px-4 py-4 bg-violet-100 flex flex-col gap-5 shadow-lg"
+      className="px-4 py-4 bg-violet-100 flex flex-col gap-5 shadow-lg min-h-screen"
       style={{
         maxWidth: '1920px',
         margin: '0 auto',
