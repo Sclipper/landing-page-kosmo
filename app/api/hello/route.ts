1af42c24f7bbb6b1fa9da88110ffa45860752dfc
clipper <mkopoi3@gmail.com>
2024-06-03 15:05:07 +0300

    Test get and post

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  return NextResponse.json({ message: 'You are already subscribed' })
+  return NextResponse.json({ message: 'POST Method' })
+}
+export async function GET(req: Request) {
+  return NextResponse.json({ message: 'GET method' })
 }
