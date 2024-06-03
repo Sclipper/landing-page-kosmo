7b4b702bae36be4bdc15222695fb201c140aa660
clipper <mkopoi3@gmail.com>
2024-06-03 15:27:24 +0300

    back to capital

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server'
 
-export async function post(req: Request) {
+export async function POST(req: Request) {
   return NextResponse.json({ message: 'POST Method' })
 }
-export async function get(req: Request) {
+export async function GET(req: Request) {
   try {
     return NextResponse.json({ message: 'GET method' })
   } catch (error) {
