44720f7711a99a417793bebd0cf1d756672251c3
clipper <mkopoi3@gmail.com>
2024-06-03 15:26:32 +0300

    change to lowercase

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function post(req: Request) {
   return NextResponse.json({ message: 'POST Method' })
 }
-export async function GET(req: Request) {
+export async function get(req: Request) {
   try {
     return NextResponse.json({ message: 'GET method' })
   } catch (error) {
