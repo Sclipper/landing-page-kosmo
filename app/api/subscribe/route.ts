513eef90afa4585707204788a05b3d74764966e6
clipper <mkopoi3@gmail.com>
2024-06-03 15:17:57 +0300

    audience stuff

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -2,10 +2,10 @@ import type { NextApiResponse } from 'next'
 import mailchimp from '@mailchimp/mailchimp_marketing'
 import { NextResponse } from 'next/server'
 
-const API_KEY = process.env.NEXT_EMAIL_KEY!
+const API_KEY = process.env.NEXT_PUBLIC_EMAIL_KEY!
 const mailchimpClient = require('@mailchimp/mailchimp_transactional')(API_KEY)
 
-const LIST_ID = process.env.NEXT_AUDIENCE_ID!
+const LIST_ID = process.env.NEXT_PUBLIC_AUDIENCE_ID!
 const DATACENTER = API_KEY?.split('-')?.[1]
 
 mailchimp.setConfig({
