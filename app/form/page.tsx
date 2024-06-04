14494dfd5d16cb26fd8d413cdd6f6562fdc779cd
clipper <mkopoi3@gmail.com>
2024-06-04 10:06:49 +0300

    Small changes

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -108,7 +108,6 @@ function FormPage() {
                 type="text"
                 id="jobTitle"
                 name="jobTitle"
-                required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
